5b01c1d7bd7e1dece417710cd85e3e82dddab306
Chase Miller <chmille4@gmail.com>
2015-06-25 10:08:28 -0600

    add first test file

---
+++ test/viz.test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var iobio = require('../src/viz.js')
+
+describe("Viz", function() {
+	
+    describe("Basic viz setup", function() {
+
+        it("creates an x axis", function() {
+          var b = 'hi';
+          expect(b).toEqual('hi');
+        });
+    });
+    
+});
+
