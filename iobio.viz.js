6df578fa4980dee1bd903cd9d4b04fda653145b5
Chase Miller <chmille4@gmail.com>
2016-04-29 22:35:49 -0400

    add some tests

--- iobio.viz.js
+++ iobio.viz.js
@@ -1433,7 +1433,7 @@ var gene = function() {
         borderRadius = 1,
         utrHeight = undefined,
         cdsHeight = undefined,
-        arrowHeight = undefined,    
+        arrowHeight = undefined,
         start = function(d) { return d.start; },
         end = function(d) { return d.end; },
         title = function(d) { return d.transcript_id; };
@@ -1446,7 +1446,7 @@ var gene = function() {
         .yAxis(null)
         .xValue(function(d) { return start(d); })
         .yValue(function(d,i) { return i; })
-        .wValue(function(d) { return end(d) - start(d); })          
+        .wValue(function(d) { return end(d) - start(d); })
 
     function chart(selection, opts) {
         // Merge defaults and options
@@ -1459,32 +1459,32 @@ var gene = function() {
         cdsHeight = cdsHeight || trackHeight;
 
         // Call base chart
-        base.call(this, selection, options)                    
+        base.call(this, selection, options)
 
         // Grab base functions for easy access
         var x = base.x(),
             y = base.y(),
             id = base.id();
             xValue = base.xValue(),
-            yValue = base.yValue(),     
+            yValue = base.yValue(),
             wValue = base.wValue(),
             color = base.color(),
-            transitionDuration = base.transitionDuration();            
+            transitionDuration = base.transitionDuration();
 
         // Grab Container
-        var g = selection.select('g.iobio-container').classed('iobio-gene', true); // grab container to draw into (created by base chart) 
+        var g = selection.select('g.iobio-container').classed('iobio-gene', true); // grab container to draw into (created by base chart)
 
         // Move Axis up
-        g.select('.iobio-axis').attr('transform', 'translate(0,-25)');    
+        g.select('.iobio-axis').attr('transform', 'translate(0,-25)');
 
 
         // Draw
-        // enter        
+        // enter
         var transcript = g.selectAll('.transcript')
                 .data(selection.datum())
         // exit
-        transcript.exit().remove()           
-            
+        transcript.exit().remove()
+
         // enter
         transcript.enter().append('g')
                 .attr('class', 'transcript')
@@ -1495,10 +1495,10 @@ var gene = function() {
             .enter().append('line')
                 .attr('class', 'reference')
                 .attr('x1', function(d) { return x(d[0])})
-                .attr('x2', function(d) { return x(d[1])})                    
+                .attr('x2', function(d) { return x(d[1])})
                 .attr('y1', trackHeight/2)
                 .attr('y2', trackHeight/2);
-        
+
         transcript.selectAll('.name').data(function(d) { return [[start(d), title(d)]] })
             .enter().append('text')
                 .attr('class', 'name')
@@ -1508,14 +1508,14 @@ var gene = function() {
                 .attr('alignment-baseline', 'middle')
                 .text( function(d) { return d[1]; } )
                 .style('fill-opacity', 0)
-        
+
         transcript.selectAll('.arrow').data(centerSpan)
             .enter().append('path')
                 .attr('class', 'arrow')
-                .attr('d', centerArrow);      
-        
-        transcript.selectAll('.feature').data(function(d) { 
-            return d['features'].filter( function(d) { var ft = d.feature_type.toLowerCase(); return ft == 'utr' || ft == 'cds';}) 
+                .attr('d', centerArrow);
+
+        transcript.selectAll('.feature').data(function(d) {
+            return d['features'].filter( function(d) { var ft = d.feature_type.toLowerCase(); return ft == 'utr' || ft == 'cds';})
         }).enter().append('g')
                 .attr('class', function(d) { return d.feature_type.toLowerCase() + ' feature';})
                 .style('fill', color )
@@ -1525,46 +1525,46 @@ var gene = function() {
                     .attr('x', function(d) { return x(d.start)})
                     .attr('width', function(d) { return x(d.end) - x(d.start)})
                     .attr('y', trackHeight /2)
-                    .attr('height', 0); 
+                    .attr('height', 0);
 
-        // update 
+        // update
         transcript.transition()
-                .duration(700)
+                .duration(transitionDuration)
                 .attr('transform', function(d,i) { return "translate(0," + y(i) + ")"});
 
         transcript.selectAll('.reference').transition()
-            .duration(700)
+            .duration(transitionDuration)
             .attr('x1', function(d) { return x(d[0])})
             .attr('x2', function(d) { return x(d[1])});
 
         transcript.selectAll('.arrow').transition()
-            .duration(700)
+            .duration(transitionDuration)
             .attr('d', centerArrow);
 
         transcript.selectAll('.name').transition()
-            .duration(700)
+            .duration(transitionDuration)
             .attr('x', function(d) { return x(d[0])-5; })
-            .attr('y', trackHeight/2)   
+            .attr('y', trackHeight/2)
             .text( function(d) { return d[1]; } )
             .style('fill-opacity', 1);
 
-        transcript.selectAll('.feature').select('rect').sort(function(a,b){ return parseInt(a.start) - parseInt(b.start)})
-            .transition()        
-                .duration(700)
+        transcript.selectAll('.feature').selectAll('rect').sort(function(a,b){ return parseInt(a.start) - parseInt(b.start)})
+            .transition()
+                .duration(transitionDuration)
                 .attr('x', function(d) { return x(d.start)})
                 .attr('width', function(d) { return x(d.end) - x(d.start)})
-                .attr('y', function(d) { 
-                    if(d.feature_type.toLowerCase() =='utr') return (trackHeight - utrHeight)/2; 
+                .attr('y', function(d) {
+                    if(d.feature_type.toLowerCase() =='utr') return (trackHeight - utrHeight)/2;
                     else return (trackHeight - cdsHeight)/2; })
-                .attr('height', function(d) { 
-                    if(d.feature_type.toLowerCase() =='utr') return utrHeight; 
-                    else return cdsHeight; });         
+                .attr('height', function(d) {
+                    if(d.feature_type.toLowerCase() =='utr') return utrHeight;
+                    else return cdsHeight; });
 
-        // Add tooltip on hover      
-        if (tooltip) {   
-            var tt = d3.select('.iobio-tooltip')    
+        // Add tooltip on hover
+        if (tooltip) {
+            var tt = d3.select('.iobio-tooltip')
             utils.tooltipHelper(transcript.selectAll('.utr,.cds'), tt, tooltip);
-        } 
+        }
 
     }
     // Rebind methods in base.js to this chart
@@ -1580,7 +1580,7 @@ var gene = function() {
     }
 
     // updates the hash with the center of the biggest span between features
-    function centerSpan(d) {    
+    function centerSpan(d) {
         var span = 0;
         var center = 0;
         var sorted = d.features
@@ -1593,22 +1593,22 @@ var gene = function() {
                 span = currSpan;
                 center = parseInt(sorted[i].end) + span/2;
             }
-        }      
+        }
         d.center = center;
-        return [d]; 
+        return [d];
     }
 
     // generates the arrow path
     function centerArrow(d) {
         var x = chart.x();
         var arrowHead = parseInt(d.strand + '5');
-        var pathStr = "M ";            
+        var pathStr = "M ";
         pathStr += x(d.center) + ' ' + (trackHeight - arrowHeight)/2;
         pathStr += ' L ' + parseInt(x(d.center)+arrowHead) + ' ' + trackHeight/2;
         pathStr += ' L ' + x(d.center) + ' ' + parseInt(trackHeight + arrowHeight)/2;
         return pathStr;
     }
-  
+
     chart.trackHeight = function(_) {
         if (!arguments.length) return trackHeight;
         trackHeight = _;
@@ -1632,7 +1632,7 @@ var gene = function() {
         arrowHeight = _;
         return chart;
     };
-    
+
 
     chart.start = function(_) {
         if (!arguments.length) return start;
--- test/viz/alignment.test.js
+++ test/viz/alignment.test.js
@@ -3,29 +3,37 @@
 var d3 = require('d3'),
     iobio = require('../../src/viz.js');
 
+var m = [15, 35, 20, 150],
+        w = 400,
+        h = 200;
+
+var data1 = [{start:1, end:3, id:'1'}, {start:2, end:4, id:'2'}, {start:3, end:5, id:'3'},{start:4, end:6, id:'4'}];
+var div = d3.select("body").append("div").attr('class', 'alignmentchart');
+
+var pileup = iobio.viz.layout.pileup().sort(null).size(w + m[1] + m[3]);
+var chart = iobio.viz.alignment()
+    .width(w + m[1] + m[3])
+    .height(h + m[0] + m[2])
+    .margin({top: m[0], right: m[1], bottom: m[2], left:m[3]})
+    .xValue(function(d) { return d.x; })
+    .yValue(function(d) { return d.y; })
+    .wValue(function(d) { return d.w; })
+    .transitionDuration(0)
+    .id(function(d) { return 'read-' + d.data.id; })
+    .on('click', function(d){})
+    .tooltip(function(d){});
+
+var p = pileup(data1);
+var selection = div.datum( p );
+chart(selection);
+
 describe("alignment", function() {
 
-    var m = [15, 35, 20, 150],
-            w = 400,
-            h = 200;
-
-    var data1 = [{start:1, end:3, id:'1'}, {start:2, end:4, id:'2'}, {start:3, end:5, id:'3'},{start:4, end:6, id:'4'}];
-    var div = d3.select("body").append("div");
-
-    var pileup = iobio.viz.layout.pileup().sort(null).size(w + m[1] + m[3]);
-    var chart = iobio.viz.alignment()
-        .width(w + m[1] + m[3])
-        .height(h + m[0] + m[2])
-        .margin({top: m[0], right: m[1], bottom: m[2], left:m[3]})
-        .xValue(function(d) { return d.x; })
-        .yValue(function(d) { return d.y; })
-        .wValue(function(d) { return d.w; })
-        .id(function(d) { return 'read-' + d.data.id; })
-        .tooltip(function(d) { return "id:  " + d.data.id + "<br/>" + "pos: " + d.data.start + ' - ' + d.data.end })
-        .on('click', function(d) { return 'hi'; })
-    var p = pileup(data1);
-    var selection = div.datum( p );
-    chart(selection);
+    // Even transitions with 0 duration take 17ms
+    // So delay all tests for 50 ms so transitions can finish
+    beforeAll(function(done) {
+        window.setTimeout(function(){done();},50);
+    });
 
     it("the id is being set on elems", function() {
         expect(document.getElementById('read-2')).not.toEqual(null);
@@ -40,7 +48,66 @@ describe("alignment", function() {
     });
 
     describe("read", function() {
-        // Add tests for x,y positions and height width
-    })
+        it("is being drawn with the right x value", function() {
+            var r = d3.select('#read-2 polygon').node().getBBox();
+            expect(r.x).toEqual(-80);
+        });
 
-})
+        it("is being drawn with the right y value", function() {
+            var r = d3.select('#read-2 polygon').node().getBBox();
+            expect(r.y).toEqual(-2);
+        });
+
+        it("is being drawn with the right width", function() {
+            var r = d3.select('#read-2 polygon').node().getBBox();
+            expect(r.width).toEqual(160);
+        });
+
+        it("is being drawn with the right height", function() {
+            var r = d3.select('#read-2 polygon').node().getBBox();
+            expect(r.height).toEqual(4);
+        });
+    })
+});
+
+// describe("alignment", function() {
+//     // Even transitions with 0 duration take 17ms
+//     // So delay all tests for 50 ms so transitions can finish
+//     beforeAll(function(done) {
+//         window.setTimeout(function(){done();},50);
+//     });
+
+//     it("the id is being set on elems", function() {
+//         expect(document.getElementById('read-2')).not.toEqual(null);
+//     });
+
+//     it("the tooltip is being set", function() {
+//         expect(typeof d3.select('#read-2').on('mouseover')).toEqual('function');
+//     });
+
+//     it("the event is being set", function() {
+//         expect(typeof d3.select('#read-2').on('click')).toEqual('function');
+//     });
+
+//     describe("read", function() {
+//         var r = document.getElementById('read-2');
+//         var bb = d3.select('#read-2 polygon').node().getBBox();
+//         console.log('bb.w = ' + bb.width);
+//         console.log('x = ' + r.getAttribute('x'));
+//         it("is being drawn with the right x value", function() {
+//           expect(r.getAttribute('x')).toEqual('80');
+//         });
+
+//         it("is being drawn with the right y value", function() {
+//           expect(r.getAttribute('y')).toEqual('98');
+//         });
+
+//         it("is being drawn with the right width", function() {
+//           expect(r.getAttribute('width')).toEqual('160');
+//         });
+
+//         it("is being drawn with the right height", function() {
+//           expect(r.getAttribute('height')).toEqual('4');
+//         });
+//     })
+// })
