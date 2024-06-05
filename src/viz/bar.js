ef06358ddda88815df841a7e367c692b47a0a940
Anders Pitman <&ers@apitman.com>
2024-06-05 16:01:35 -0600

    Switch to implicit d3 import

--- src/viz/bar.js
+++ src/viz/bar.js
@@ -1,4 +1,4 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as d3 from 'd3';
 import baseModule from './base.js';
 import utils from '../utils.js';
 
--- src/viz/base.js
+++ src/viz/base.js
@@ -1,5 +1,4 @@
-//import * as d3 from 'd3';
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+import * as d3 from 'd3';
 import utils from '../utils.js';
 
 const styles = `
